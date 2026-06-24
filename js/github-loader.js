58b2e0a119bf61a6fb013cd04657baa8bbe5294d
nemeth-a-attila <nemeth.a.attila@gmail.com>
2026-06-24 13:54:58 +0200

    Debug

--- js/github-loader.js
+++ js/github-loader.js
@@ -1,13 +1,31 @@
 const GITHUB_OWNER = "nemeth-a-attila";
 const GITHUB_REPO = "turaterkep";
 
+//async function getFolderContents(path) {
+//
+//    const response = await fetch(
+//        `https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents/${path}`
+//    );
+//
+//    return await response.json();
+//}
+
 async function getFolderContents(path) {
 
-    const response = await fetch(
-        `https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents/${path}`
-    );
+    const url =
+        `https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents/${path}`;
 
-    return await response.json();
+    console.log("Lekérés:", url);
+
+    const response = await fetch(url);
+
+    console.log("Status:", response.status);
+
+    const data = await response.json();
+
+    console.log("Válasz:", data);
+
+    return data;
 }
 
 async function getPlannedTracks() {
